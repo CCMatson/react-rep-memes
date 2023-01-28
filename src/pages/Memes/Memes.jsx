5a60ef9ae76a8341ba0c67b3a58e1ae194a9f1fa
CCMatson <catherine.matson@gmail.com>
2023-01-27 20:15:15 -0800

    add nav bar

---
+++ src/pages/Memes/Memes.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const Memes = () => {
+  const [memes, setMemes] = useState([])
+
+  useEffect(() => {
+
+  })
+
+  return (
+    <>
+      <h3>Memes are fun</h3>
+    </>
+  );
+}
+
+export default Memes;
